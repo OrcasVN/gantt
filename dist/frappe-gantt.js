e08a00700ca21238e2ad269f3f3100e6c225c746
vientran199 <vientran1999@gmail.com>
2023-10-10 11:14:13 +0700

    update

--- dist/frappe-gantt.js
+++ dist/frappe-gantt.js
@@ -1036,7 +1036,7 @@ var Gantt = (function () {
                     if (column.events && Array.isArray(column.events)) {
                         column.events.forEach(event => {
                             $.on(container, event.type, event.target, (ev) => {
-                                event.handler(task, column);
+                                event.handler(task, column, this.tasks);
                             });
                         });
                     }
