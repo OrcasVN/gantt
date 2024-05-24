e3728ea754a5b9b3a38b7494be11463e1eb765cf
vientran199 <vientran1999@gmail.com>
2024-05-24 16:13:53 +0700

    hightlight current week, month

--- dist/frappe-gantt.js
+++ dist/frappe-gantt.js
@@ -1531,6 +1531,52 @@ class Gantt {
                 append_to: this.layers.grid,
             });
         }
+
+        if (this.view_is(VIEW_MODE.WEEK)) {
+            const x =
+                Math.floor(date_utils.diff(date_utils.today(), this.gantt_start, 'day') /
+                    7) *
+                this.options.column_width;
+            const y = 0;
+
+            const width = this.options.column_width;
+            const height =
+                (this.options.bar_height + this.options.padding) *
+                this.tasks.length +
+                this.options.header_height +
+                this.options.padding / 2;
+
+            createSVG('rect', {
+                x,
+                y,
+                width,
+                height,
+                class: 'today-highlight',
+                append_to: this.layers.grid,
+            });
+        }
+        if (this.view_is(VIEW_MODE.MONTH)) {
+            const x = date_utils.diff(date_utils.today(), this.gantt_start, 'month') *
+                this.options.column_width;
+
+            const y = 0;
+
+            const width = this.options.column_width;
+            const height =
+                (this.options.bar_height + this.options.padding) *
+                this.tasks.length +
+                this.options.header_height +
+                this.options.padding / 2;
+
+            createSVG('rect', {
+                x,
+                y,
+                width,
+                height,
+                class: 'today-highlight',
+                append_to: this.layers.grid,
+            });
+        }
     }
 
     make_dates() {
